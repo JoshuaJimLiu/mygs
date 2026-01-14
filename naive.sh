24926876fd505c3a5ac43db2dbad9a1781828b9b
JoshuaJimLiu <jimliu20200918@gmail.com>
2026-01-14 22:53:48 +0800

    add rgb vs gray training

--- naive.sh
+++ naive.sh
@@ -4,3 +4,7 @@ mkdir -p log
 python train.py -s ./data/bicycle/ | tee -a log/bicycle.log
 
 python train.py -s ./data/bonsai/  | tee -a log/bonsai.log
+
+python train.py -s ./data/bicycle/ --color_loss "rgb" | tee -a log/bicycle_rgb.log
+
+python train.py -s ./data/bonsai/ --color_loss "rgb" | tee -a log/bonsai_rgb.log
