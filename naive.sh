abd68c22bb31d193f5fbec17350a324ef5c439fc
JoshuaJimLiu <jimliu20200918@gmail.com>
2026-01-16 20:14:05 +0800

    fix log

--- naive.sh
+++ naive.sh
@@ -6,7 +6,7 @@ for dir in "bicycle" "bonsai" "counter" "garden" "kitchen" "room" "stump"
 do
 python train.py -s ./data/$dir/ --eval | tee -a log/$dir.log
 
-python train.py -s ./data/$dir/ --eval --color_loss "rgb" | tee -a log/$dir_rgb.log 
+python train.py -s ./data/$dir/ --eval --color_loss "rgb" | tee -a log/${dir}_rgb.log
 
 done
 
